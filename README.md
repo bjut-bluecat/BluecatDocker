4f6f2cbf0ab2a505b9c72bd7d01220e63e7fc621
yuang.shen <yuang.shen@mintegral.com>
2020-11-27 01:37:47 +0800

    add docker

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # BluecatDocker
+
+运行方法<br>
+g++ main.cpp -o docker -std=c++11<br>
+sudo ./docker<br> 
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "docker.hpp"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "start docker..." << std::endl;
+    bluecat::docker_info doc_info;
+    doc_info.hostname = "蓝猫的docker";
+    doc_info.root_dir="./bluecat";
+    bluecat::bluecat_docker mydocker(doc_info);
+    mydocker.start();                  // 启动容器
+    std::cout << "stop docker..." << std::endl;
+    return 0;
+}
